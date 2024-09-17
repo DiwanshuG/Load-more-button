022e7a612d0f3c674ed5588e31a6e05d6e81f4e2
Diwanshu Gangwar <118631453+DiwanshuG@users.noreply.github.com>
2024-09-17 14:45:18 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import LoadMoreData from "./components/Load-more-button";
+function App() {
+  return (
+    <div className="App">
+      {/* Load-more component  */}
+
+      <LoadMoreData/>
+    </div>
+  );
+}
+
+export default App;
